1d6d5346673411de61eddbdac10790b3cfc0d53d
Obi Victor <victorfrancis612@gmail.com>
2022-03-28 09:49:05 +0100

    updated

--- main.js
+++ main.js
@@ -13,4 +13,15 @@ $(document).ready(function () {
         .show("1000");
     }
   });
+  //Add active to btn
+  $(".filter-item").click(function () {
+    $(this).addClass("active-filter").siblings().removeClass("active-filter");
+  });
+});
+
+//Header Background change on scroll
+let header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle('shadow', window.scrollY > 0);
 });
